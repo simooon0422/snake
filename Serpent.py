ea4b7717d2c9c478739df043231c8c3e8db73d36
Szymon Hudziak <szymonh.dg@gmail.com>
2023-11-12 21:42:24 +0100

    replace using rect center with xy coordinates in Serpent class, add directions dictionary

--- Serpent.py
+++ Serpent.py
@@ -4,6 +4,7 @@ import pygame
 class Serpent(pygame.sprite.Sprite):
     """Class for the snake character"""
     def __init__(self, game):
+        """Initialize snake sprite in the game"""
         super().__init__()
 
         self.screen = game.screen
@@ -19,6 +20,27 @@ class Serpent(pygame.sprite.Sprite):
         # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
 
-        self.rect.center = (self.screen_width / 2, self.screen_height / 2)
+        # Coordinates of snake's rect
+        self.rect.x = self.screen_width / 2
+        self.rect.y = self.screen_height / 2
 
+        # Speed of snake
+        self.speed = 5
+
+        # Direction of moving
+        self.directions = {"left": (-1, 0), "right": (0, 1), "down": (0, 1), "up": (0, -1), "stop": (0, 0)}
+        # self.directions = [[0, 0], [-1, 0], [0, 1], [0, 1], [0, -1]]
+        self.current_direction = self.directions["down"]
+        # Display snake
+        self.screen.blit(self.image, self.rect)
+
+        self.update()
+
+    def update(self):
+        """Update position of the snake"""
+        print(self.rect.center)
+        self.rect.x = self.rect.x + self.current_direction[0]
+        self.rect.y = self.rect.y + self.current_direction[1]
+        print(self.rect.center)
+        self.screen.fill((0, 0, 0))
         self.screen.blit(self.image, self.rect)
