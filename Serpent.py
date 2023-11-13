7e486908b4a544daf3a948f9722ba9e98671b3de
Szymon Hudziak <szymonh.dg@gmail.com>
2023-11-13 12:19:16 +0100

    replace creating body in Serpent class with list of BodyModule objects

--- Serpent.py
+++ Serpent.py
@@ -1,7 +1,8 @@
 import pygame
+import BodyModule
 
 
-class Serpent(pygame.sprite.Sprite):
+class Serpent:
     """Class for the snake character"""
     def __init__(self, screen):
         """Initialize snake sprite in the game"""
@@ -10,19 +11,12 @@ class Serpent(pygame.sprite.Sprite):
         self.screen = screen
         self.screen_width, self.screen_height = pygame.display.get_surface().get_size()
 
-        # Color of the snake
-        self.green = (0, 255, 0)
+        self.body = [BodyModule.BodyModule(self.screen)]
 
-        # Create an image of the snake and fill it with a color.
-        self.image = pygame.Surface([50, 50])
-        self.image.fill(self.green)
+        self.body[0].rect.x = self.screen_width / 2
+        self.body[0].rect.y = self.screen_height / 2
 
-        # Fetch the rectangle object that has the dimensions of the image
-        self.rect = self.image.get_rect()
-
-        # Coordinates of snake's rect
-        self.rect.x = self.screen_width / 2
-        self.rect.y = self.screen_height / 2
+        self.body[0].screen.blit(self.body[0].image, self.body[0].rect)
 
         # Speed of snake
         self.speed = 5
@@ -30,14 +24,8 @@ class Serpent(pygame.sprite.Sprite):
         # Direction of moving
         self.directions = {"left": (-1, 0), "right": (1, 0), "down": (0, 1), "up": (0, -1), "stop": (0, 0)}
 
-        # Display snake
-        self.screen.blit(self.image, self.rect)
-
-        self.update("up")
-
     def update(self, command):
         """Update position of the snake"""
-        self.rect.move_ip(self.directions[command])
+        self.body[0].rect.move_ip(self.directions[command])
         self.screen.fill((0, 0, 0))
-        self.screen.blit(self.image, self.rect)
-
+        self.screen.blit(self.body[0].image, self.body[0].rect)
